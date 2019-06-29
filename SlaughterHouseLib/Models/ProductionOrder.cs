f0cba860a52da9973ae214ed0a64da9d5d51566a
boyhardaway <boy.hardaway@gmail.com>
2019-06-30 01:12:43 +0700

    add ProductionOrder model

---
+++ SlaughterHouseLib/Models/ProductionOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SlaughterHouseLib.Models
+{
+    public class ProductionOrder
+    {
+ 
+        public List<ProductionOrderItem> ProductionOrderItem { get; set; }
+
+    }
+
+}
---
+++ SlaughterHouseLib/Models/ProductionOrderItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SlaughterHouseLib.Models
+{
+    public class ProductionOrderItem
+    {
+         
+    }
+
+
+}
