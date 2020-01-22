2e16515266a4547611602333422c4686c554fe13
damrongc <damrong.c@cpf.co.th>
2020-01-22 19:29:39 +0700

    add server version

--- SlaughterHouseServer/Form_Main.Designer.cs
+++ SlaughterHouseServer/Form_Main.Designer.cs
@@ -107,9 +107,9 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(392, 41);
+            this.label1.Size = new System.Drawing.Size(570, 41);
             this.label1.TabIndex = 2;
-            this.label1.Text = "ระบบบริหารจัดการโรงชำแหละสุกร";
+            this.label1.Text = "ระบบบริหารจัดการโรงชำแหละสุกร v.20.01.22.001";
             // 
             // plMenu
             // 
